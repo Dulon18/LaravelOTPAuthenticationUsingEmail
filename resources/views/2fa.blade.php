0dbbf5a262f9ae0c08d7b82ce56584a83e9fb500
Dulon18 <dulon587@gmail.com>
2023-02-04 16:24:21 +0600

    OTP authentication

--- resources/views/2fa.blade.php
+++ resources/views/2fa.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Laravel 9 Two Factor Authentication Using Email</div>
+                <div class="card-header bg-primary text-white fw-bold">Otp authentication Using Email</div>
   
                 <div class="card-body">
                     <form method="POST" action="{{ route('2fa.store') }}">
@@ -51,7 +51,7 @@
   
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
-                                <a class="btn btn-link" href="{{ route('2fa.resend') }}">Resend Code?</a>
+                                <a class="btn text-success" href="{{ route('2fa.resend') }}">Resend Code?</a>
                             </div>
                         </div>
   
